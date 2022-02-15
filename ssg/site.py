a0c01cbd91a84830ee5bc98a1659176e155a01b6
Floris Weijler <Floris.weijler@ing.com>
2022-02-15 14:33:27 +0100

    exercise1

--- ssg/site.py
+++ ssg/site.py
@@ -1,10 +1,11 @@
 from msilib.schema import Directory
 from pathlib import Path
 
+
 class Site:
     def __init__(self, source, dest):
-        self.source = Path()
-        self.dest = Path()
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
