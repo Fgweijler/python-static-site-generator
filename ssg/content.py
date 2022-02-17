0ce2c1a7254f30d1c2e72408c449466208be2b17
Floris Weijler <Floris.weijler@ing.com>
2022-02-17 15:43:34 +0100

    123

--- ssg/content.py
+++ ssg/content.py
@@ -5,7 +5,7 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 
-class Content:
+class Content(Mapping):
     __delimeter = r"^(?:-|\+){3}\s*$"
     __regex= re.compile(__delimeter, re.MULTILINE)
 
