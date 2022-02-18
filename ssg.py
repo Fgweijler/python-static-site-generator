d9dab78b816be034f8fffa6de32a64bb34036693
Floris Weijler <Floris.weijler@ing.com>
2022-02-18 15:58:49 +0100

    123

--- ssg.py
+++ ssg.py
@@ -1,20 +1,20 @@
-import ssg.parsers
 import typer
-from ssg.site import Site 
+from ssg.site import Site
 
-def main(source= "content",dest= "dist"):
+import ssg.parsers
+
+
+def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
         "parsers": [
             ssg.parsers.ResourceParser(),
             ssg.parsers.MarkdownParser(),
-            ssg.parsers.RestructuredTextParser(),
+            ssg.parsers.ReStructuredTextParser(),
         ],
     }
     Site(**config).build()
 
 
 typer.run(main)
-
-
